8fb8201fa6e71f9807153095dfb122d3f7c864dc
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-10-06 12:00:19 -0300

    Implement App Bar with Drawer

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,19 +1,23 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+    <nav>
+      <v-app-bar flat app elevation=4>
+        <v-app-bar-nav-icon  class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>      
+        <v-toolbar-title class="headline ">
+          <span>Demonstração </span>
+          <span class="font-weight-light">Painel Administrativo</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn 
+          text
+          href="https://deyvidjlira.com">
+          <span class="mr-2">Contate-me</span>
+        </v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer app v-model="drawer" class="blue-grey darken-4">
+      </v-navigation-drawer>
+    </nav>
 
     <v-content>
       <slot />
@@ -24,5 +28,10 @@
 <script>
 export default {
   name: 'default',
+  data() {
+    return {
+      drawer: false
+    }
+  }
 };
 </script>
